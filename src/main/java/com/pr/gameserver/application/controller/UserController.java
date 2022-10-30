c56177a63ed2cc04135e6c98d606cd13420380cb
Hicham Naoufal <7191269+hnaoufal@users.noreply.github.com>
2022-10-30 18:06:07 +0100

    feat(mysql): finish connection to DB

--- src/main/java/com/pr/gameserver/application/controller/UserController.java
+++ src/main/java/com/pr/gameserver/application/controller/UserController.java
@@ -1,17 +1,33 @@
 package com.pr.gameserver.application.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.pr.gameserver.domain.entries.User;
+import com.pr.gameserver.infrastructre.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
-    @GetMapping("/")
-    public String index() {
-        return "Greetings from Hicham";
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path="/add") // Map ONLY POST Requests
+    public @ResponseBody String addNewUser (@RequestParam String name
+            , @RequestParam String email) {
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        // This returns a JSON or XML with the users
+        return userRepository.findAll();
     }
 
     @GetMapping("/login")
-    public String index2() {
-        return "Hicham Login";
+    public String index() {
+        return "Greetings from Hicham";
     }
 }
